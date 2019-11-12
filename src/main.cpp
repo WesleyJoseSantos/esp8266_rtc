56a74f5b761382e8dcc2edf03ad8cc4b90922e89
WESLEY JOSE SANTOS <53940899+WesleyJoseSantos@users.noreply.github.com>
2019-11-12 12:24:11 -0300

    Atualização dos comentários do código

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,9 @@
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
 
-
 //Parâmetros do WiFi
-const char* wifi_ssid = "cabo canaveral";
-const char* wifi_pass = "05151924";
+const char* wifi_ssid = "nome do seu wifi";
+const char* wifi_pass = "senha do seu wifi";
 
 //Lista de servidores ntp:
 const char* pool_server = "pool.ntp.org";
@@ -23,8 +22,8 @@ void setup() {
       delay(500);
   }
   //A configuração do relógio RTC, deve ser chamada após a conexão com a rede WiFi:
-  configTime(-3*GMT, 0, pool_server, 
-                        google_server,
+  configTime(-3*GMT, 0, pool_server,     //Aqui é passado o ajuste de fuso horário (-3GMT para o Brasil)
+                        google_server,   //e a lista de links dos provedores ntp's a serem utilizados
                         time_server);    //Após essa configuração, a conexão com internet não é mais necessária. 
 }
 
